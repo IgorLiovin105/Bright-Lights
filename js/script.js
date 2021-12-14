33400891f895c752957b607c724b6a52f37f476c
IgorLiovin105 <igorliovin07@gmail.com>
2021-12-15 00:15:44 +0600

    animations on scroll

--- js/script.js
+++ js/script.js
@@ -37,3 +37,40 @@ if(menuLinks.length > 0) {
         }
     }
 }
+
+const animItems = document.querySelectorAll('._anim__items');
+
+if(animItems.length > 0) {
+    window.addEventListener("scroll", animOnScroll);
+    function animOnScroll() {
+        for(let index = 0; index < animItems.length; index++) {
+            const animItem = animItems[index];
+            const animItemHeight = animItem.offsetHeight;
+            const animItemOffset = offset(animItem).top;
+            const animStart = 10;
+
+            let animItemPoint = window.innerHeight - animItemHeight / animStart;
+            if(animItemHeight > window.innerHeight) {
+                animItemPoint = window.innerHeight - awindow.innerHeight / animStart;
+            }
+
+            if((scrollY > animItemOffset - animItemPoint) && scrollY < (animItemOffset + animItemHeight)) {
+                animItem.classList.add('_active');
+            }
+            else {
+                if(!animItem.classList.contains('_anim_no_hide')) {
+                    animItem.classList.remove('_active');
+                }
+            }
+        }
+    }
+    function offset(el) {
+        const rect = el.getBoundingClientRect(),
+            scrollLeft = window.scrollX || document.documentElement.scrollLeft,
+            scrollTop = window.scrollY || document.documentElement.scrollTop;
+        return { top: rect.top + scrollTop, left: rect.left + screenLeft }
+    }
+    setTimeout(() => {
+        animOnScroll();
+    }, 500);
+}
